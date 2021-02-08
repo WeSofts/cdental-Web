3d28a32b0914cabb4b0b1aad265db9f8ecfd220c
Jose Kabo <kabo.0699@gmail.com>
2021-02-07 23:55:33 -0600

    change local to online

--- src/app/services/app/servicios/servicios.service.ts
+++ src/app/services/app/servicios/servicios.service.ts
@@ -16,24 +16,24 @@ export class ServiciosService {
   ) {}
 
   insertService(body: any): Observable<any>{
-    return this.http.post(`${this.appConfig.APP_ENDPOINT_LOCAL}dentista/add-service`, body);
+    return this.http.post(`${this.appConfig.APP_ENDPOINT}dentista/add-service`, body);
   }
   insertSubService(body: any): Observable<any>{
-    return this.http.post(`${this.appConfig.APP_ENDPOINT_LOCAL}dentista/add-sub-service`, body);
+    return this.http.post(`${this.appConfig.APP_ENDPOINT}dentista/add-sub-service`, body);
   }
   deleteSubService(idSubService: string): Observable<any>{
-    return this.http.post(`${this.appConfig.APP_ENDPOINT_LOCAL}dentista/delete-sub-service`, {id_SubServicio: idSubService});
+    return this.http.post(`${this.appConfig.APP_ENDPOINT}dentista/delete-sub-service`, {id_SubServicio: idSubService});
   }
   updatingSubService(body: any): Observable<any>{
-    return this.http.put(`${this.appConfig.APP_ENDPOINT_LOCAL}dentista/update-sub-service`, body);
+    return this.http.put(`${this.appConfig.APP_ENDPOINT}dentista/update-sub-service`, body);
   }
   getServicios(idClinica: string): Observable<any>{
-    return this.http.post(this.appConfig.APP_ENDPOINT_LOCAL + 'dentista/all-services-by-clinic', {id_clinica: idClinica});
+    return this.http.post(this.appConfig.APP_ENDPOINT + 'dentista/all-services-by-clinic', {id_clinica: idClinica});
   }
   getServiciosYSubServicios(idClinica: string): Observable<any>{
-    return this.http.post(this.appConfig.APP_ENDPOINT_LOCAL + 'dentista/all-services-subservices-clinic', {id_clinica: idClinica});
+    return this.http.post(this.appConfig.APP_ENDPOINT + 'dentista/all-services-subservices-clinic', {id_clinica: idClinica});
   }
   getSubServicios(idServicio: string): Observable<any>{
-    return this.http.post(this.appConfig.APP_ENDPOINT_LOCAL + 'dentista/all-sub-service-by-service', {id_servicio: idServicio});
+    return this.http.post(this.appConfig.APP_ENDPOINT + 'dentista/all-sub-service-by-service', {id_servicio: idServicio});
   }
 }
